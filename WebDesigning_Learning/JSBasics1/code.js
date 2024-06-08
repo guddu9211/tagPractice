13ef536f26676eb97674adaef000439d1b8a0d4b
Manish Kumar Prajapati <myindiaman@gmail.com>
2024-06-08 16:33:14 +0530

    some testing code added

--- WebDesigning_Learning/JSBasics1/code.js
+++ WebDesigning_Learning/JSBasics1/code.js
@@ -1,3 +1,17 @@
+function manip(arr) {
+    arr.push(5);
+    arr = [9,8,7];
+    return arr;
+}
+
+let li = [1,2,3,4];
+manip(li);
+console.log(li);
+
+li = manip(li);
+console.log(li);
+
+
 // console.log("ye lo mai aa gaya");
 
 // function createRectangle(len, bre) {
@@ -14,12 +28,12 @@
 // }
 
 
-let a = {value: 10};
+// let a = {value: 10};
 
-function inc(a){
-    a.value++;
-}
+// function inc(a){
+//     a.value++;
+// }
 
-inc(a);
+// inc(a);
 
-console.log(a.value);
+// console.log(a.value);
